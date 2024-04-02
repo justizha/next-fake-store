f64a5c2f5569453f84ccd36d3bd6e790e57facfb
justizha <alfathizhaaaaaaa@gmail.com>
2024-04-02 10:58:46 +0700

    update

--- src/app/category/page.tsx
+++ src/app/category/page.tsx
@@ -1,12 +1,18 @@
-async function getCategories(){
-    const category = await fetch("https://fakestoreapi.com/products");
+async function getCategories() {
+  const res = await fetch("https://fakestoreapi.com/products/categories");
+
+  return res.json();
 }
 
-
 export default async function Category() {
+  const categories: any = await getCategories();
   return (
     <section>
-      <article></article>
+      <article>
+        {categories.map((category: any) => (
+          <div key={category}>{category}</div>
+        ))}
+      </article>
     </section>
   );
 }
--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,19 +1,19 @@
 import { use } from "react";
 
 async function getProduct(id: number) {
-  const res = await fetch(`https://fakestoreapi.com/products/${id}`);
+	const res = await fetch(`https://fakestoreapi.com/products/${id}`);
 
-  return res.json();
+	return res.json();
 }
 
 export default function Page({ params }: { params: { id: number } }) {
-  const product = use(getProduct(params.id));
-  return (
-    <section>
-      <article className="lg:px-20">
-        <h2 className="text-2xl font-semibold">{product.title}</h2>
-        <p className="text-base">{product.description}</p>
-      </article>
-    </section>
-  );
+	const product = use(getProduct(params.id));
+	return (
+		<section>
+			<article className="lg:px-20 py-24">
+				<h2 className="text-2xl font-semibold">{product.title}</h2>
+				<p className="text-base">{product.description}</p>
+			</article>
+		</section>
+	);
 }
